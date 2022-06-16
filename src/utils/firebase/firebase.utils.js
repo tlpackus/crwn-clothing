6bd1622d7141203065cc533d8fba1b263ac1134d
Theron Packus <tlpackus@gmail.com>
2022-06-16 12:46:26 -0700

    add firebase SDK into package.json and crete new utils folder to house the initialization javascript for imbedding the SDK

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+// import { getAuth,
+//         signInWithRedirect,
+//         signInWithPopep,
+//         GoogleAuthProvider } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4K1ewyyausx_-wqFIpGeJVhOql1CP7b0",
+  authDomain: "crwn-clothing-db-6b24e.firebaseapp.com",
+  projectId: "crwn-clothing-db-6b24e",
+  storageBucket: "crwn-clothing-db-6b24e.appspot.com",
+  messagingSenderId: "777468606497",
+  appId: "1:777468606497:web:5a336b4255eabcbb588b60"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+// const provider = new GoogleAuthProvider();
+
+// provider.setCustomParameters({
+//   prompt: "select_account"
+// })
