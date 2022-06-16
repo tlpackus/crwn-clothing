641b120f3c9cf5016ed8edf1bd3caef2729c2ba4
Theron Packus <tlpackus@gmail.com>
2022-06-16 10:18:39 -0700

    add functionality to use navigation component for routing proper links through shop and home

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,18 @@
 import { Fragment } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
-        <h1>I am the nav bar</h1>
+        <Link className='logo-container' to='/'>
+          <div>Logo</div>
+        </Link>
+        <div className='nav-links-container'>
+          <Link className= 'nav-link' to='/shop'>
+            SHOP
+          </Link>
+        </div>
       </div>
       <Outlet />
     </Fragment>
