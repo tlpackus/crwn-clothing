ab023b52a9ec08a05d6b0d64791574801a5e63c1
Theron Packus <tlpackus@gmail.com>
2022-06-17 10:20:14 -0700

    remove logic for google redirect sign up option so project is back to useing google pop up specifically

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,9 @@
-import { auth,
+import { 
     signInWithGooglePopup,
     createUserDocumentFromAuth,
-    signInWithGoogleRedirect, 
 } from "../../utils/firebase/firebase.utils";
 
-import { useEffect } from 'react';
-import { getRedirectResult } from "firebase/auth";
-
 const SignIn = () => {
-  useEffect(() => {
-    async function fetchData() {
-      const response = await getRedirectResult(auth);
-    console.log(response);
-    }
-    fetchData()
-  }, []);
-
   const logGoogleUser = async () => {
     const {user} = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -25,7 +13,6 @@ const SignIn = () => {
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button>
     </div>
   )
 };
